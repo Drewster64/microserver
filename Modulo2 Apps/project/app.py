0284df475dd3810502512d93bd7af173519a5bda
Drew <slimpsycho2187@gmail.com>
2025-04-18 16:56:12 -0400

    modulo 2 leccion 1 -- version 2

--- Modulo2 Apps/project/app.py
+++ Modulo2 Apps/project/app.py
@@ -14,5 +14,15 @@ def tareas():
     ]
     return render_template('tareas.html', tareas=tareas)
 
+@app.route('/empleados')
+def empleados():
+    empleados_info = [
+        {"nombre": "Carlos Pérez", "rol": "Carpintero", "foto": "employee1.jpg"},
+        {"nombre": "Ana López", "rol": "Supervisora", "foto": "supervisor.jpg"},
+        {"nombre": "Luis Ramírez", "rol": "Carpintero", "foto": "employee2.jpg"},
+        {"nombre": "James Díaz", "rol": "Mantenimiento", "foto": "employee3.webp"}
+    ]
+    return render_template('empleados.html', empleados=empleados_info)
+
 if __name__ == '__main__':
     app.run(debug=True)
